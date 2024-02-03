829f3e2163523c72fe62ee561ea768a9118a5ca5
Хаджимурат Капышев <kapklas@gmail.com>
2024-02-03 23:08:01 +0600

    cummit

---
+++ Assets/scriptes/ui/Sceneswit.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneswit : MonoBehaviour
+{
+    public void GoToSampleScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/scriptes/ui/zdohnut.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zdohnut : MonoBehaviour
+{
+    void QuitButton()
+    {
+        Debug.Log("Game is exiting");
+        Application.Quit();
+    }
+}
