5f08ff246d025b856fa3398abb4f81611cd554c5
Ispamlord <146575918+Ispamlord@users.noreply.github.com>
2024-02-04 02:48:58 +0700

    shoting Enemy

--- Assets/scriptes/Shoting.cs
+++ Assets/scriptes/Shoting.cs
@@ -6,29 +6,7 @@ using UnityEngine.PlayerLoop;
 
 public class Shoting : MonoBehaviour
 {
-    //public Transform firePoint;  // Позиция, откуда будет выпущена стрела
-    //public GameObject arrowPrefab;  // Префаб стрелы
-    //public float arrowForce = 20f;  // Сила стрельбы
-
-    //// Функция для стрельбы
-    //void Shoot()
-    //{
-    //    // Получаем позицию мыши в мировых координатах
-    //    Vector3 mousePosition = Input.mousePosition;
-    //    mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-    //    // Создаем экземпляр стрелы из префаба
-    //    GameObject arrow = Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
-
-    //    // Получаем компонент Rigidbody2D стрелы
-    //    Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
-
-    //    // Вычисляем направление от firePoint к позиции мыши
-    //    Vector2 shootDirection = (mousePosition - firePoint.position).normalized;
-
-    //    // Придаем стреле силу в указанном направлении
-    //    rb.AddForce(shootDirection * arrowForce, ForceMode2D.Impulse);
-    //}
+    
     public Transform firePoint;
     public GameObject arrowPrefab;
     public float arrowForce = 20f;
@@ -37,9 +15,10 @@ public class Shoting : MonoBehaviour
     {
         // Создаем экземпляр стрелы из префаба
         GameObject arrow = Instantiate(arrowPrefab, firePoint.position, firePoint.rotation);
-
+       
         // Получаем компонент Rigidbody2D стрелы
         Rigidbody2D rb = arrow.GetComponent<Rigidbody2D>();
+        
 
         // Придаем стреле силу в указанном направлении
         rb.AddForce(shootDirection * arrowForce, ForceMode2D.Impulse);
---
+++ Assets/scriptes/Sunduk.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sunduk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision != null)
+        {
+
+        }   
+    }
+    public void open()
+    {
+
+    }
+}
