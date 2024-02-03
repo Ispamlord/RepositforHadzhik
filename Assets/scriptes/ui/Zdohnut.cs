43d041b628d262b384a55ff4dd87d8829bdd255c
MC91 <mrchingiz91@gmail.com>
2024-02-04 00:58:58 +0600

    Rename to upper-case

--- Assets/scriptes/ui/Zdohnut.cs
+++ Assets/scriptes/ui/Zdohnut.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class zdohnut : MonoBehaviour { 
+public class Zdohnut : MonoBehaviour { 
     void QuitButton()
     {
             Debug.Log("Game is exiting");
