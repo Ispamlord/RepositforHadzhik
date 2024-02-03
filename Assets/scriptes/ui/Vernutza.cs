bb1b79460e7d29e8c4166ca9e2f04508ed578c20
Хаджимурат Капышев <kapklas@gmail.com>
2024-02-04 02:32:19 +0600

    new menu

---
+++ Assets/scriptes/ui/Vernutza.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace scriptes.UI
+{
+    public class Vernutza : MonoBehaviour
+    {
+        public void GoToMAIN()
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
+
+
