94c2cf1cd7558a48c0d0b0dd3e1faba3824f4873
MC91 <mrchingiz91@gmail.com>
2024-02-03 21:16:02 +0600

    Rotated a weapon by 90 degrees

--- Assets/scriptes/WeaponeController.cs
+++ Assets/scriptes/WeaponeController.cs
@@ -9,17 +9,13 @@ public class WeaponeController : MonoBehaviour
     private Vector2 lookDirection;
     void Update()
     {
-        // ѕолучаем позицию мыши в мировых координатах
         mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
-        // ¬ычисл€ем направление от оружи€ к позиции мыши
         lookDirection = (mousePosition - transform.position).normalized;
 
-        // ¬ычисл€ем угол поворота в радианах
-        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90;
 
-        // ѕоворачиваем оружие в соответствии с углом
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 }
