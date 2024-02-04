b88a068408ed7ece79d1fc656caf72047cdf9bd5
Хаджимурат Капышев <kapklas@gmail.com>
2024-02-04 21:55:14 +0600

    ne rabotaet govno

---
+++ Assets/scriptes/procedural gen/WObj.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WObj 
+{
+    public Vector2 Postition;
+    public Vector2 Direction;
+    public float ChanceToAlter;
+    public WObj(Vector2 pos, Vector2 dir, float ChanceToAlter)
+    {
+        Postition = pos;
+        Direction = dit;
+        ChanceToAlter=CTA;
+    }
+}
