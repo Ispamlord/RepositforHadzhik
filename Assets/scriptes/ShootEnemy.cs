917426848adcd046e77a366824f4168c54f2815f
MC91 <mrchingiz91@gmail.com>
2024-02-04 22:10:43 +0600

    Minor refactors

--- Assets/scriptes/ShootEnemy.cs
+++ Assets/scriptes/ShootEnemy.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ShootEnemy : MonoBehaviour
 {
+    public GameObject owner;
     public Transform target;
     public Shoting shoot;
-    public bool CD= false;
-    public float couldown = 5f;
-    Vector2 shot;
+    public bool CD = false;
+    public float cooldown = 5f;
+    
+    private Vector2 shot;
+    
     // Start is called before the first frame update
     private void OnTriggerStay2D(Collider2D collision)
     {
+        
         if (collision.name == target.name)
         {
             shot = collision.transform.position;
@@ -19,14 +24,13 @@ public class ShootEnemy : MonoBehaviour
     }
     private void Update()
     {
-        if (!CD)
-        {
-            fire();
-            CD = true;
-            Invoke("ResetMagic", couldown);
-        }
+        if (CD) return;
+        Fire();
+        CD = true;
+        Invoke(nameof(ResetMagic), cooldown);
     }
-    public void fire()
+
+    private void Fire()
     {
          Vector2 direction = target.position - transform.position;
          float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
