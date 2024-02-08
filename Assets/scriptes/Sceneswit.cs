f1091b1bfec08f664d58b1e134979201cf1513b8
MC91 <mrchingiz91@gmail.com>
2024-02-08 14:55:30 +0600

    camelCasification

--- Assets/scriptes/Sceneswit.cs
+++ Assets/scriptes/Sceneswit.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 namespace scriptes{
     public class Sceneswit : MonoBehaviour{
-        public int sceneid;
+        public int sceneId;
         public void GoToSampleScene(){
-            SceneManager.LoadScene(sceneid);
+            SceneManager.LoadScene(sceneId);
         }
     }
 }
