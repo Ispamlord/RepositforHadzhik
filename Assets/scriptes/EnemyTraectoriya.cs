57ff831d04e47ea9a080ef15cf30353ff8e2165a
Ispamlord <146575918+Ispamlord@users.noreply.github.com>
2024-02-28 01:07:38 +0700

    fix enemytraectoriya

--- Assets/scriptes/EnemyTraectoriya.cs
+++ Assets/scriptes/EnemyTraectoriya.cs
@@ -6,26 +6,26 @@ public class EnemyTraectoriya : MonoBehaviour
 {
     public Transform target; // ѕерсонаж, на которого будет направлено оружие
 
-    //private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (target != null && collision.name == target.name)
+        {
+            Target();
+        }
+    }
+    private void Target()
+    {
+        Vector3 targetDirection = target.position - transform.position;
+        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+    //void Update()
     //{
-    //    if (target != null && collision.name == target.name)
+    //    if (target != null)
     //    {
-    //        Target();
-    //    }
-    //}
-    //private void Target()
-    //{
     //        Vector3 targetDirection = target.position - transform.position;
     //        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90;
     //        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    //    }
     //}
-    void Update()
-    {
-        if (target != null)
-        {
-            Vector3 targetDirection = target.position - transform.position;
-            float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-        }
-    }
 }
