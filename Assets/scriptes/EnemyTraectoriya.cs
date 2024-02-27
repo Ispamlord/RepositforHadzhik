ebf42aa29efcf9f34aa40cc882b9db63236d8dd1
Ispamlord <146575918+Ispamlord@users.noreply.github.com>
2024-02-28 00:35:51 +0700

    fixni Shingis plz

--- Assets/scriptes/EnemyTraectoriya.cs
+++ Assets/scriptes/EnemyTraectoriya.cs
@@ -6,12 +6,25 @@ public class EnemyTraectoriya : MonoBehaviour
 {
     public Transform target; // ѕерсонаж, на которого будет направлено оружие
 
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (target != null && collision.name == target.name)
+    //    {
+    //        Target();
+    //    }
+    //}
+    //private void Target()
+    //{
+    //        Vector3 targetDirection = target.position - transform.position;
+    //        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90;
+    //        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    //}
     void Update()
     {
         if (target != null)
         {
             Vector3 targetDirection = target.position - transform.position;
-            float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg-90;
+            float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90;
             transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
         }
     }
